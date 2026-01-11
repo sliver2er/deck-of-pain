c586600f4516475f109c336a8829c2dc8dd2b277
sliver2er <shpaul2001@snu.ac.kr>
2026-01-12 02:38:43 +0900

    init

---
+++ granite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@apps-in-toss/web-framework/config";
+
+export default defineConfig({
+  appName: "deck-of-pain",
+  brand: {
+    displayName: "deck-of-pain", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    icon: "logo/logo.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+  },
+  web: {
+    host: "192.168.123.132",
+    port: 5173,
+    commands: {
+      dev: "vite --host",
+      build: "vite build",
+    },
+  },
+  permissions: [],
+  outdir: "dist",
+});
